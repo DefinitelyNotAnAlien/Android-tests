213feeb7b277b77a7b979989300c8255a4a2c5fe
Alien (Laptop) <ivesjosue@gmail.com>
2019-11-12 13:48:59 -0600

    Changed spinner to add items programatically

--- CapturaTest/app/src/main/java/com/mycompany/capturatest/MainActivity.java
+++ CapturaTest/app/src/main/java/com/mycompany/capturatest/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,9 +20,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Add items to aircraft selector
+        Spinner spinner = findViewById(R.id.aircraftSelector);
         List<String> aircraft = Arrays.asList((getResources().getStringArray(R.array.aircraft_array)));
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_dropdown_item, aircraft);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+        spinner.setAdapter(spinnerAdapter);
 
         // Add items to Recycler View
         RecyclerView recyclerView = findViewById(R.id.logItems);
@@ -29,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
         List<LogItem> items = new ArrayList<>();
-        for (int i = 1; i <= 3; i++)
-            items.add(new LogItem(i));
+        // Maintenance log should only have 3 entry items
+        for (int i = 1; i <= 3; i++) items.add(new LogItem(i));
         LogAdapter adapter = new LogAdapter(items);
         recyclerView.setAdapter(adapter);
     }
